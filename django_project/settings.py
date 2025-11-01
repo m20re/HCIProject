31f3d07315137bdb045ed6db3a11d9d360b66f23
Kurtis <kurtiskathol27@gmail.com>
2025-10-31 19:15:54 -0500

    Add audio translation

--- django_project/settings.py
+++ django_project/settings.py
@@ -4,6 +4,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 
 # Loads environmental variables
 load_dotenv()
